83ea68afca8f5ef598608eac92868a43c03a2dc0
Imad Ahmed <erimadahmad@gmail.com>
2019-06-07 16:33:01 +0530

    Version 1 Commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // console.log("Ready!")
     navigator.getBattery().then(function(battery) {
-        console.log(battery.level*100);
+        // console.log(battery.level*100);
         var level = battery.level*100;
         var showBatteryLevel = document.getElementById("percentage")
         showBatteryLevel.innerHTML = level;
         // ... and any subsequent updates.
         battery.onlevelchange = function() {
           console.log(this.level);
+          showBatteryLevel.innerHTML = level;
         };
       });
   })
+
+
